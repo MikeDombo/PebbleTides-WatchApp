c513078b3735f2a367f4f9b35b2f2406e200f65a
Michael Dombrowski <mikepi3.14@gmail.com>
2014-04-22 16:42:44 -0400

    bug fixes

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,17 +1,17 @@
 function parseTide(response, name){
 //setup Vars
+		var responseMessage;	
 		var tideLevel;
 		var tideTime;
 		var city;
 		var state;
 		var currTime = Math.round(Date.now()/1000);
-        if (response.success = "true") {
-          tideLevel = response.response[0].periods[0].type;
-		  tideTime = response.response[0].periods[0].timestamp;
-		  city = ((response.response[0].place.name).substring(0,1)).toUpperCase()+(response.response[0].place.name).substring(1,(response.response[0].place.name).length);
-		  state = (response.response[0].place.state).toUpperCase();
-		  var diffTime = tideTime - currTime;
-		var responseMessage;
+        if (response.success === true) {
+        tideLevel = response.response[0].periods[0].type;
+		tideTime = response.response[0].periods[0].timestamp;
+		city = ((response.response[0].place.name).substring(0,1)).toUpperCase()+(response.response[0].place.name).substring(1,(response.response[0].place.name).length);
+		state = (response.response[0].place.state).toUpperCase();
+		var diffTime = tideTime - currTime;
 		var tide;
 		var tideTimemin = ((diffTime)/60);
 		var tideTimehr = (tideTimemin-(tideTimemin%60))/60;
@@ -20,11 +20,11 @@ function parseTide(response, name){
 		var minute;
 //Decide on grammar/english to use
 		if(Math.abs(tideTimemin==1)){minute=" minute";}
-		else {minute=" minutes"}
-		if (Math.abs(tideTimehr) > 1){hour = " hours"}
-		else {hour = " hour"}
-		if(tideLevel == 'h'){var tide = "High tide";}
-		else {var tide = "Low tide";}
+		else {minute=" minutes";}
+		if (Math.abs(tideTimehr) > 1){hour = " hours";}
+		else {hour = " hour";}
+		if(tideLevel == 'h'){tide = "High tide";}
+		else {tide = "Low tide";}
 		if(tideTimemin <= 0){
 			timePassed[0] = " was ";
 			timePassed[1] = " ago";}
@@ -75,71 +75,72 @@ function parseTide(response, name){
 	}
 		print();
 }
-		
+
 function print(){
 	simply.style("large");
 	simply.scrollable(true);
 	var responseMessage;
-	if(sessionStorage.gpsResp != null){
+	if(sessionStorage.gpsResp !== null){
 	responseMessage = sessionStorage.gpsResp;	
 	}
-	if(sessionStorage.zip1Resp != null){
+	if(sessionStorage.zip1Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip1Resp;	
 	}
-	if(sessionStorage.zip2Resp != null){
+	if(sessionStorage.zip2Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip2Resp;	
 	}
-	if(sessionStorage.zip3Resp != null){
+	if(sessionStorage.zip3Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip3Resp;	
 	}
-	if(sessionStorage.zip4Resp != null){
+	if(sessionStorage.zip4Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip4Resp;	
 	}
-	if(sessionStorage.zip5Resp != null){
+	if(sessionStorage.zip5Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip5Resp;	
 	}
-	if(sessionStorage.zip6Resp != null){
+	if(sessionStorage.zip6Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip6Resp;	
 	}
-	if(sessionStorage.zip7Resp != null){
+	if(sessionStorage.zip7Resp !== null){
 	responseMessage = responseMessage+sessionStorage.zip7Resp;	
 	}
 	simply.body(responseMessage);
 	simply.vibe();
 	//Pebble.showSimpleNotificationOnPebble("Pebble Tides", responseMessage);
-	
+
 }
 function getTides(lat, lng) {
 	console.log("called getTides with lat lng");
-  var response;
+	var response;
 	var name = "gps";
-  var req = new XMLHttpRequest();
+	var req = new XMLHttpRequest();
 	req.onload = function(e) {
        console.log(req.responseText);
        response = JSON.parse(req.responseText);
-		if(response!=null && req.status == 200){
+		if(response!==null && req.status == 200){
 			sessionStorage.gpsDone=true;
 			parseTide(response, name);
 		}
-	}
-  req.open('GET', "http://api.aerisapi.com/tides/closest?p=" + lat +","+ lng + "&client_id=eOQzJRTGPtPKdfokmpGQ9&client_secret=Elmx32lruftjejQDJWmyAgd1FMEp98LHHk9aVasg&radius=1000mi&from=-5hours&to=+6hours");
+	};
+  req.open('GET', "http://api.aerisapi.com/tides/closest?p=" + lat +","+ lng + "&client_id=eOQzJRTGPtPKdfokmpGQ9&client_secret=Elmx32lruftjejQDJWmyAgd1FMEp98LHHk9aVasg&radius=1000mi&from=-8hours&plimit=2&psort=dt");
   req.send(null);	
 }
-	  
+
 function getTidesZip(zip, name) {
+	zip = zip.replace(/\s+/g, "%20");
 	console.log("called getTides with zip: "+zip);
-  var response;
-  var req = new XMLHttpRequest();
+	var response;
+	var req = new XMLHttpRequest();
 	req.onload = function(e) {
        console.log(req.responseText);
        response = JSON.parse(req.responseText);
-		if(response!=null && req.status == 200){
+		if(response!==null && req.status == 200){
 			parseTide(response, name);
 		}
-	}
+	};
   req.open('GET', "http://api.aerisapi.com/tides/closest?p=" + zip + "&client_id=eOQzJRTGPtPKdfokmpGQ9&client_secret=Elmx32lruftjejQDJWmyAgd1FMEp98LHHk9aVasg&radius=1000mi&from=-8hours&plimit=2&psort=dt");
   req.send(null);
-	  }
+}
 
 function showPosition(position) {
     sessionStorage.lat = position.coords.latitude;
@@ -159,30 +160,30 @@ function runPos() {
 	sessionStorage.zip5Resp="";
 	sessionStorage.zip6Resp="";
 	sessionStorage.zip7Resp="";
-	
+
 	if(localStorage.useGPS == "on"){
-		sessionStorage.gpsDone=false;
+		localStorage.gpsDone=false;
 		navigator.geolocation.getCurrentPosition(showPosition);
 	}
-	if(localStorage.zip1 != ""){
+	if(localStorage.zip1 !== ""){
 		getTidesZip(localStorage.zip1, "zip1");
 	}
-	if(localStorage.zip2 != ""){
+	if(localStorage.zip2 !== ""){
 		getTidesZip(localStorage.zip2, "zip2");
 	}
-	if(localStorage.zip3 != ""){
+	if(localStorage.zip3 !== ""){
 		getTidesZip(localStorage.zip3, "zip3");
 	}
-	if(localStorage.zip4 != ""){
+	if(localStorage.zip4 !== ""){
 		getTidesZip(localStorage.zip4, "zip4");
 	}
-    if(localStorage.zip5 != ""){
+    if(localStorage.zip5 !== ""){
 		getTidesZip(localStorage.zip5, "zip5");
 	}
-	if(localStorage.zip6 != ""){
+	if(localStorage.zip6 !== ""){
 		getTidesZip(localStorage.zip6, "zip6");
 	}
-	if(localStorage.zip7 != ""){
+	if(localStorage.zip7 !== ""){
 		getTidesZip(localStorage.zip7, "zip7");
 	}
 }
@@ -207,12 +208,22 @@ Pebble.addEventListener("showConfiguration", function() {
 
 Pebble.addEventListener("webviewclosed", function(e) {  
 	console.log("configuration closed, webview");
-  var options = JSON.parse(decodeURIComponent(e.response));
-  console.log("Options = " + JSON.stringify(options));
+	var options = JSON.parse(decodeURIComponent(e.response));
+	console.log("Options = " + JSON.stringify(options));
 	if(options.gps == "on" || options.gps == "off"){
 		setUp(options);
 	}
 });
+
+Pebble.addEventListener("configurationClosed", function(e) {  
+	console.log("configuration closed, config closed");
+	var options = JSON.parse(decodeURIComponent(e.response));
+	console.log("Options = " + JSON.stringify(options));
+	if(options.gps == "on" || options.gps == "off"){
+		setUp(options);
+	}
+});
+
 
 simply.on('singleClick', function(e) {
 	if(e.button == "select"){
@@ -226,6 +237,7 @@ simply.on('longClick', function(e) {
 simply.scrollable(true);
 simply.style("small");
 simply.setText({
-  title: 'Pebble Tides',
-   body: 'Press \'Select\' to Get Tides.\n\nCurrent Configuration:\nGPS is '+localStorage.useGPS+'\nZip 1: '+localStorage.zip1+'\nZip 2: '+localStorage.zip2+'\nZip 3: '+localStorage.zip3+'\nZip 4: '+localStorage.zip4+'\nZip 5: '+localStorage.zip5+'\nZip 6: '+localStorage.zip6+'\nZip 7: '+localStorage.zip7,
+   title: 'Pebble Tides',
+   body: 'Press \'Select\' to Get Tides.\n\nCurrent Configuration:\nGPS is '+localStorage.useGPS+'\nZip 1: '+localStorage.zip1+'\nZip 2: '+localStorage.zip2+'\nZip 3: '+localStorage.zip3+'\nZip 4: '+localStorage.zip4+'\nZip 5: '+localStorage.zip5+'\nZip 6: '+localStorage.zip6+'\nZip 7: '+localStorage.zip7+
+	'\n\nBy Michael Dombrowski\nMikeDombrowski.com\n\nVersion 2.2.4',
 }, true);
